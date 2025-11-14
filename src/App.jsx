9f502726bab222be10e96fced61712efff4b7da6
Luis Alberto <oluiscruz25@gmail.com>
2025-11-14 01:22:47 -0300

    origin

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Conteudo from './pages/content'
+import './App.scss'
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <Conteudo />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/content.jsx
@@ -0,0 +1,56 @@
+import Perfil from '../assets/Perfil.jpg';
+import { FaWhatsapp } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaTiktok } from "react-icons/fa";
+import Verificar from '../assets/Verificar.png'
+
+
+import '../App.scss';
+
+const Conteudo = () => {
+    return (
+
+        <div className="container-content">
+            <div className="box">
+                <div className="box-header">
+
+
+                    <img
+                        src={Perfil}
+                        alt="Foto de perfil"
+                        className='img-profile' />
+
+                    <div className="title">
+                        <h1>Eduarda Martins <i>
+                            <img src={Verificar} alt="" /></i></h1>
+
+                        <div className="bio">
+                            <p>Mais do que unhas, é auto-estima. Designer que cuida e encanta. Fale comigo! 💖</p>
+                        </div>
+                    </div>
+                </div>
+
+                <nav className="links">
+                    <li><i>
+                        <FaWhatsapp />
+                    </i>
+                        <a href="https://wa.me/5581984410216?text=Olá, gostaria de mais informações sobre seu trabalho!">Agende seu horário agora <span>📲</span></a></li>
+                    <li>
+                        <i>
+                            <FaTiktok />
+
+                        </i>
+                        <a href="https://www.tiktok.com/@dumatss?_r=1&_t=ZS-91NkrXtHsXf">Tiktok <span>✨💋</span></a></li>
+                    <li>
+                        <i>
+                            <FaInstagram />
+
+                        </i>
+                        <a href="https://www.instagram.com/dumartinstudio/">Instagram <span>📸💅</span></a></li>
+                </nav>
+            </div>
+        </div>
+    )
+}
+
+export default Conteudo;
